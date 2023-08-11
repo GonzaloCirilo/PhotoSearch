97985e2a823c95b9ec17e83f276c6441712e77ea
Gonzalo Cirilo <gonzalo_cirilo@hotmail.com>
2023-08-11 00:22:26 -0500

    Fix search call and add safe search option

--- data/src/main/java/com/example/data/remote/PhotoSearchApi.kt
+++ data/src/main/java/com/example/data/remote/PhotoSearchApi.kt
@@ -10,6 +10,7 @@ interface PhotoSearchApi {
     suspend fun getTrendingPhotos(
         @Query("method") method: String = FlickerApiConstants.GET_RECENT_PHOTOS_METHOD,
         @Query("extras") extraInfo: String = "owner_name,date_upload",
+        @Query("user_id") userId: String = "user-id",
         @Query("per_page") itemsPerPage: Int,
         @Query("page") page: Int,
     ): PhotosWrapperDto
@@ -18,9 +19,10 @@ interface PhotoSearchApi {
     suspend fun searchPhotos(
         @Query("method") method: String = FlickerApiConstants.SEARCH_PHOTOS_METHOD,
         @Query("extras") extraInfo: String = "owner_name,date_upload",
+        @Query("text") searchText: String,
+        @Query("safe_search") safeSearch: Int = 1,
         @Query("per_page") itemsPerPage: Int,
         @Query("page") page: Int,
-        @Query("text") searchText: String,
     ): PhotosWrapperDto
 
 
